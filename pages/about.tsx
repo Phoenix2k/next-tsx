7ae3b681f6c745414fe7a059c19952b4c0232c63
Niko Pettersen <niko.pettersen@gmail.com>
2020-01-15 14:34:36 +0200

    refactor(pages): Add navigation to pages

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,3 +1,10 @@
-const About = () => <h1>About page</h1>;
+import Nav from '../components/Nav';
+
+const About = () => (
+  <>
+    <h1>About page</h1>
+    <Nav />
+  </>
+);
 
 export default About;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,10 @@
-const Home = () => <h1>Hello world!</h1>;
+import Nav from '../components/Nav';
+
+const Home = () => (
+  <>
+    <h1>Hello world!</h1>
+    <Nav />
+  </>
+);
 
 export default Home;
