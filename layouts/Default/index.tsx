81f873a83aa8575cd0973ccfe1869f04750b900d
Niko Pettersen <niko.pettersen@gmail.com>
2020-01-15 17:31:24 +0200

    feat(build): Add Emotion + Babel config

---
+++ layouts/Default/index.tsx
@@ -0,0 +1,33 @@
+import styled from '@emotion/styled';
+import { css, Global } from '@emotion/core';
+
+import Nav from '../../components/Nav';
+
+const LayoutContainer = styled.div`
+  border: 1px solid #ddd;
+  margin: 1em;
+  padding: 1em;
+`;
+
+const DefaultLayout = (props: any) => (
+  <>
+    <Global
+      styles={css`
+        body,
+        html {
+          font-family: Helvetica, Arial, sans-serif;
+          margin: 0;
+          padding: 0;
+        }
+      `}
+    />
+    <LayoutContainer>
+      <header>Header</header>
+      <Nav />
+      <main>{props.children}</main>
+      <footer>Footer</footer>
+    </LayoutContainer>
+  </>
+);
+
+export default DefaultLayout;
