485ddf7e42cf3c1c76753e89912b6dcbc137afc2
Niko Pettersen <niko.pettersen@gmail.com>
2020-01-27 11:54:58 +0200

    feat(build): Add document setup

---
+++ src/pages/_document.tsx
@@ -0,0 +1,29 @@
+import Document, {
+  DocumentContext,
+  DocumentInitialProps,
+  Head,
+  Html,
+  Main,
+  NextScript
+} from 'next/document';
+
+class NextDocument extends Document {
+  static async getInitialProps(context: DocumentContext): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(context);
+    return { ...initialProps };
+  }
+
+  render(): JSX.Element {
+    return (
+      <Html lang="en">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default NextDocument;
