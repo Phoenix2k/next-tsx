3df5c1d409c2ddab55521fc4d82a5051273b1619
Niko Pettersen <niko.pettersen@gmail.com>
2020-01-27 11:42:27 +0200

    feat(pages): Add meta tags and page title

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,7 +1,13 @@
+import Head from 'next/head';
 import DefaultLayout from '../../layouts/Default';
 
 const About = (): JSX.Element => (
   <DefaultLayout>
+    <Head>
+      <title>About page</title>
+      <meta name="description" content="About page description" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
     <h1 itemProp="title">About page</h1>
   </DefaultLayout>
 );
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,7 +1,13 @@
+import Head from 'next/head';
 import DefaultLayout from '../../layouts/Default';
 
 const Home = (): JSX.Element => (
   <DefaultLayout>
+    <Head>
+      <title>Home page</title>
+      <meta name="description" content="Home page description" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
     <h1 itemProp="title">Home page</h1>
   </DefaultLayout>
 );
