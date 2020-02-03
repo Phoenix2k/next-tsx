ac844c8a86d72f244eee10d53b686bcb7e41c2c6
Niko Pettersen <niko.pettersen@gmail.com>
2020-02-03 14:26:45 +0200

    feat(build): Add error template

---
+++ src/pages/_error.tsx
@@ -0,0 +1,50 @@
+import { NextPageContext } from 'next';
+import Head from 'next/head';
+import Link from 'next/link';
+import React from 'react';
+import styled from '../theme';
+
+const Container = styled.main`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  width: 100vw;
+
+  h1 {
+    font-size: 2em;
+    margin: 0;
+    padding: 0;
+  }
+
+  h2 {
+    font-size: 6em;
+    margin: 0.5rem;
+  }
+`;
+
+interface ErrorProps {
+  statusCode?: number | null;
+}
+
+const ErrorPage = ({ statusCode }: ErrorProps): JSX.Element => (
+  <Container>
+    <Head>
+      <title>{statusCode && `${statusCode} - `}Page not found</title>
+    </Head>
+    <h1 id="error-status-code">Error</h1>
+    <h2>
+      <code>{statusCode || 'Unknown'}</code>
+    </h2>
+    <Link href="/">
+      <a>Return to home page</a>
+    </Link>
+  </Container>
+);
+
+ErrorPage.getInitialProps = ({ res }: NextPageContext): ErrorProps => {
+  return { statusCode: (res && res.statusCode) || null };
+};
+
+export default ErrorPage;
