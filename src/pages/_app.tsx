6aa41e79f2d89a16e3d8b3d1b28fe9b0393013fe
Niko Pettersen <niko.pettersen@gmail.com>
2020-02-04 16:53:21 +0200

    feat(build): Add Web App Manifest + favicons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import Head from 'next/head';
 import React from 'react';
 import { ReactAxeConfig } from 'react-axe';
 import ReactDOM from 'react-dom';
+import manifest from '../../public/manifest.json';
 import { enableA11y, globalStyles, theme } from '../theme';
 
 interface NextAppProps {
@@ -35,6 +36,8 @@ if (
 const NextApp = ({ Component, pageProps }: NextAppProps): JSX.Element => (
   <>
     <Head>
+      <link key="manifest" rel="manifest" href="/manifest.json" />
+      <meta key="theme-color" name="theme-color" content={manifest.theme_color} />
       <meta key="viewport" name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
     <ThemeProvider theme={theme}>
