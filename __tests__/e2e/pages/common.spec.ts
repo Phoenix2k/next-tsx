b3082657fb25d57c714e68eb8cfeb9b560ec870d
Niko Pettersen <niko.pettersen@gmail.com>
2020-02-11 23:57:43 +0200

    feat(e2e): Group tests in descriptive blocks

--- __tests__/e2e/pages/common.spec.ts
+++ __tests__/e2e/pages/common.spec.ts
@@ -20,63 +20,71 @@ describe('Running generic tests for all pages...\n', () => {
         cy.visit(pageUrl);
       });
 
-      it('has a header', () => {
-        cy.get('header').should('be.visible');
+      describe('Header', () => {
+        it('has a header', () => {
+          cy.get('header').should('be.visible');
+        });
       });
 
-      it('has a navigation', () => {
-        cy.get('nav').should('be.visible');
+      describe('Navigation', () => {
+        it('has a navigation', () => {
+          cy.get('nav').should('be.visible');
+        });
+
+        it('current page is marked as active', () => {
+          cy.get(`[href="${pageUrl}"]`)
+            .first()
+            .should('have.class', activeLink.class);
+        });
+
+        it('current page link color is green', () => {
+          cy.get(`[href="${pageUrl}"]`)
+            .first()
+            .should('have.css', 'color', activeLink.color);
+        });
+
+        it('has only one active navigation link', () => {
+          cy.get('.nav-link--active').should('have.length', 1);
+        });
+
+        it('navigates to a random page and comes back', () => {
+          cy.get(`[href="${altPageUrl}"]`)
+            .first()
+            .click();
+          cy.wait(250);
+          cy.itemProp('title').contains(altPageTitle);
+          if (Cypress.config('video')) {
+            cy.screenshot(altPageSlug);
+          }
+          cy.get(`[href="${pageUrl}"]`)
+            .first()
+            .click();
+          cy.wait(250);
+          cy.itemProp('title').contains(pageTitle);
+        });
       });
 
-      it('navigation link is marked as active', () => {
-        cy.get(`[href="${pageUrl}"]`)
-          .first()
-          .should('have.class', activeLink.class);
+      describe('Main container', () => {
+        it('has a main container', () => {
+          cy.get('main').should('be.visible');
+          if (Cypress.config('video')) {
+            cy.screenshot(pageSlug);
+          }
+        });
+
+        it('has a main title', () => {
+          cy.itemProp('title').should('be.visible');
+        });
+
+        it('has the correct main title', () => {
+          cy.itemProp('title').contains(pageTitle);
+        });
       });
 
-      it('navigation link is green', () => {
-        cy.get(`[href="${pageUrl}"]`)
-          .first()
-          .should('have.css', 'color', activeLink.color);
-      });
-
-      it('has only one active navigation link', () => {
-        cy.get('.nav-link--active').should('have.length', 1);
-      });
-
-      it('navigates to a random page and comes back', () => {
-        cy.get(`[href="${altPageUrl}"]`)
-          .first()
-          .click();
-        cy.wait(250);
-        cy.itemProp('title').contains(altPageTitle);
-        if (Cypress.config('video')) {
-          cy.screenshot(altPageSlug);
-        }
-        cy.get(`[href="${pageUrl}"]`)
-          .first()
-          .click();
-        cy.wait(250);
-        cy.itemProp('title').contains(pageTitle);
-      });
-
-      it('has a main container', () => {
-        cy.get('main').should('be.visible');
-        if (Cypress.config('video')) {
-          cy.screenshot(pageSlug);
-        }
-      });
-
-      it('has a title', () => {
-        cy.itemProp('title').should('be.visible');
-      });
-
-      it('has the correct title', () => {
-        cy.itemProp('title').contains(pageTitle);
-      });
-
-      it('has a footer', () => {
-        cy.get('footer').should('be.visible');
+      describe('Footer', () => {
+        it('has a footer', () => {
+          cy.get('footer').should('be.visible');
+        });
       });
     });
   });
