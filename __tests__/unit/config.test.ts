a42d9a992170b6e3b7723d5ee12b9f4970895545
Niko Pettersen <niko.pettersen@gmail.com>
2020-02-17 16:26:14 +0200

    chore(unit): Update snapshots

--- __tests__/unit/config.test.ts
+++ __tests__/unit/config.test.ts
@@ -5,8 +5,8 @@ import config from '../../src/config';
 describe('Config', () => {
   it('matches specification', () => {
     expect(Object.keys(config).length).toEqual(3);
-    expect(Object.keys(config.pages).length).toBeGreaterThan(0)
-    expect(Object.keys(config.theme).length).toBeGreaterThan(0)
+    expect(Object.keys(config.pages).length).toBeGreaterThan(0);
+    expect(Object.keys(config.theme).length).toBeGreaterThan(0);
   });
   it('matches snapshot', () => {
     expect(config).toMatchSnapshot();
